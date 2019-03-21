e4cb263c98924f3f068bb8c3c76820100cdb6b9f
Abhishek Ezhava <Abhishekshaji32@gmail.com>
2019-03-21 13:19:35 +0530

    Update bs4 file.py

--- bs4 file.py
+++ bs4 file.py
@@ -4,5 +4,5 @@ import requests
 source=requests.get('https://medium.com').text
 
 soup =BeautifulSoup(source,'lxml')
-article=soup.find_all()
-print(article)
+article=soup.find_all()   #enter the tag u want to fetch eg find('div') or find_all()
+print(article) #mention prettify for a good format
